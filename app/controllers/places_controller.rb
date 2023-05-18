74b62fa3a8b2afb6cb0d60401538a63cfb87e689
dikssha dinesh <dikssha.dinesh@kellogg.northwestern.edu>
2023-05-18 22:57:37 +0000

    commit6

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,29 +1,29 @@
 class PlacesController < ApplicationController
 
-def index
-  @places = Place.all
-end
+  def index
+    @places = Place.all
+  end
 
- def show
-    @place = Place.find_by({"id" => params["id"] })
-    @posts = Post.where({"place_id" => @place["id"]}) #give me posts for place_id=1
-end
+  def show
+      @place = Place.find_by({"id" => params["id"] })
+      @posts = Post.where({"place_id" => @place["id"]}) #give me posts for place_id=1
+  end
 
-def new
-  @place = Place.new
-end
+  def new
+    @place = Place.new
+  end
 
-def create
-  @place= Place.new
-  @place["name"]=params["place"]["name"]
-  @place.save
-redirect_to "/places"
-end
-
-def destroy
-  @place = Place.find_by({ "id" => params["id"] })
-  @place.destroy
+  def create
+    @place= Place.new
+    @place["name"]=params["place"]["name"]
+    @place.save
   redirect_to "/places"
-end
+  end
+
+  def destroy
+    @place = Place.find_by({ "id" => params["id"] })
+    @place.destroy
+    redirect_to "/places"
+  end
 
 end
